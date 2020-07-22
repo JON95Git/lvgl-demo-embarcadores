7986da10d5c4f21f552622bd31e99f62c218a917
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2020-07-22 01:29:12 -0300

    Módulo auxiliar de sistema

---
+++ app/system/system.h
@@ -0,0 +1,11 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+#include "hardware.h"
+#include <stdio.h>
+#include "retarget.h"
+
+void system_utils_init(void);
+void error_handler(void);
+
+#endif /* __SYSTEM_H */
