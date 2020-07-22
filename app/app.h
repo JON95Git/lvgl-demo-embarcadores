448875c2d5402b42281c3605e6d2c9a00376f1d8
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2020-07-22 01:26:53 -0300

    Aplicação do usuário

---
+++ app/app.h
@@ -0,0 +1,12 @@
+#ifndef __APP_H
+#define __APP_H
+
+#include "hardware.h"
+#include "lvgl.h"
+#include "tft.h"
+#include "gui_app.h"
+#include "app_tasks.h"
+
+void app_main(void);
+
+#endif /* __APP_H */ 
